b3154c150fef7d88a12016caf09d397715a0f333
Oliver Caine <olliecaine@gmail.com>
2024-05-02 21:09:29 +1000

    Workflow build (#5)
    * Build in GitHub actions on PRs

--- apps/mobile/src/screens/Home.spec.tsx
+++ apps/mobile/src/screens/Home.spec.tsx
@@ -5,6 +5,6 @@ import { Home } from "./Home";
 describe("<App />", () => {
   it("should be able to find Hello World! text", () => {
     const { getByText } = render(<Home />);
-    expect(getByText("Hello World!")).toBeVisible();
+    expect(getByText("Hello World!")).toBeDefined();
   });
 });
