e471ce9c0744518457a0ed742ad2a2dd7004e752
Oliver Caine <olliecaine@gmail.com>
2024-05-02 20:12:44 +1000

    Expect ts error (#4)
    * Build on PRs too

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  // @ts-expect-error - TODO: remove this line and fix
   plugins: [react()],
   test: {
     environment: "jsdom",
