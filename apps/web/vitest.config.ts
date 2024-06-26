3df0d16306e5157e1acff48df03d1d753818b16f
Oliver Caine <olliecaine@gmail.com>
2024-06-26 22:29:50 +1000

    Adding @expo/metro-runtime to mobile app

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  // @ts-expect-error - TODO: remove this line and fix
   plugins: [react()],
   test: {
     environment: "jsdom",
