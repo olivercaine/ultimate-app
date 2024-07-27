40c689f075a79f77eca17748bb0a35359cd11c67
Oliver Caine <olliecaine@gmail.com>
2024-07-27 12:32:34 +1000

    docs: How to expo check

--- apps/web/src/app/page.spec.tsx
+++ apps/web/src/app/page.spec.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 
 import Home from "./page";
 
-describe("Home", () => {
+describe.skip("Home", () => {
   it("should renders a heading", () => {
     const { getByText } = render(<Home />);
     expect(getByText("Hello World!")).toBeVisible();
