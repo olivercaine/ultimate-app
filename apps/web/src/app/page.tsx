4ba32d1702f890a7e138b1ee6188c7541e920dde
Oliver Caine <olliecaine@gmail.com>
2024-08-04 21:44:36 +1000

    refactor: show web app name in ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Page() {
   return (
     <main className="grid place-items-center min-h-screen">
       <h1>Hello World!</h1>
+      <p>1.0.0</p>
+      <p>App: web</p>
     </main>
   );
 }
