fa3b3c5d1a49913c033a814046616fec77f2b49b
Oliver Caine <olliecaine@gmail.com>
2025-03-12 13:19:58 +1100

    feat(x): x
    x

--- packages/math/tools/cleanup.ts
+++ packages/math/tools/cleanup.ts
@@ -1,12 +1,14 @@
-import { readFileSync, writeFileSync } from 'fs'
+import { readFileSync, writeFileSync } from "fs";
 
-const packageJson = JSON.parse(readFileSync('./dist/package.json').toString())
-delete packageJson.devDependencies
-delete packageJson.scripts
-
-// TODO: remove "dist/" from following (as it's already going dist folder):
-// "jsnext:main": "dist/index.js",
-// "module": "dist/index.js",
-// "types": "dist/index.d.ts",
-
-writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2))
+const packageJson = JSON.parse(readFileSync("./dist/package.json").toString());
+const fieldsToUpdate = ["jsnext:main", "module", "types"];
+// eslint-disable-next-line pure/pure
+fieldsToUpdate.forEach((field) => {
+  if (packageJson[field] && packageJson[field].startsWith("dist/")) {
+    packageJson[field] = packageJson[field].replace("dist/", "");
+  }
+});
+delete packageJson["devDependencies"];
+delete packageJson["scripts"];
+delete packageJson["husky"];
+writeFileSync("./dist/package.json", JSON.stringify(packageJson, undefined, 2));
