e8535c6992cd9350031725b972fe819085829c47
Oliver Caine <olliecaine@gmail.com>
2025-03-18 08:44:38 +1100

    fix(package): Updating command to create package (#78)

--- commands.sh
+++ commands.sh
@@ -2,15 +2,18 @@
 
 # To run these functions: `. ./commands.sh && <method_name> <optional_param_1> <optional_param_2>`
 
-function add_package { # `. ./commands.sh && add_package`
-  echo "Please supply the name for the new package:"
-  read packageName
+function add_package { # `. ./commands.sh && add_package mynewpackage`
+  packageNamespace="@ultimate"
 
-  if [ -z "$packageName" ]; then
-    echo "Error: No package name entered. Exiting..."
-    exit 1
+  if [ -z "$1" ]; then
+    echo "No package name provided."
+    read -p "Please enter the package name: " packageName
+  else
+    packageName="$1"
   fi
 
   git clone https://github.com/olivercaine/typescript-library-boilerplate.git packages/$packageName && rm -rf packages/$packageName/.git
-  sed -i.bak "s/\"name\": \".*\"/\"name\": \"@olliecaine\/$packageName\"/" packages/$packageName/package.json
+  sed -i.bak "s/\"name\": \".*\"/\"name\": \"$packageNamespace\/$packageName\"/" packages/$packageName/package.json
+  rm "packages/$packageName/package.json.bak"
+  pnpm install && pnpm run build --filter $packageNamespace/$packageName
 }
