24874cf23d9b19d0727fdd1835519f4fd707f83e
Oliver Caine <olliecaine@gmail.com>
2025-03-24 11:42:50 +1100

    feat(package): bumping package

--- packages/package/rollup.config.js
+++ packages/package/rollup.config.js
@@ -1,17 +1,38 @@
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
 export default {
-  output: {
-    format: 'umd',
-    name: 'typescript-library-boilerplate'
-  },
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/typescript-library-boilerplate.bundle.js',
+      format: 'umd',
+      name: 'typescript-library-boilerplate',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig-esm.json'
+    })
+  ],
   external: [
     // put some third party libraries here
   ],
-  onwarn: ( warning ) => {
-      const skip_codes = [
-          'THIS_IS_UNDEFINED',
-          'MISSING_GLOBAL_NAME'
-      ];
-      if ( skip_codes.indexOf(warning.code) != -1 ) return;
-      console.error(warning);
+  onwarn: (warning) => {
+    const skipCodes = [
+      'THIS_IS_UNDEFINED',
+      'MISSING_GLOBAL_NAME'
+    ]
+    if (skipCodes.indexOf(warning.code) !== -1) return
+    console.error(warning)
   }
-};
+}
