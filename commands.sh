9fa8011dbc42c1924e7d13aecceef6b38c45f96d
Oliver Caine <olliecaine@gmail.com>
2025-03-25 11:24:22 +1100

    feat(clone): from version

--- commands.sh
+++ commands.sh
@@ -12,9 +12,10 @@ function add_package { # `. ./commands.sh && add_package mynewpackage`
     packageName="$1"
   fi
 
-  cp -r packages/package packages/$packageName
+  git clone --depth 0 https://github.com/olivercaine/typescript-library-boilerplate.git -b v20250325001649 packages/$packageName && rm -rf packages/$packageName/.git
+  # cp -r packages/package packages/$packageName
   sed -i.bak "s/\"name\": \".*\"/\"name\": \"$packageNamespace\/$packageName\"/" packages/$packageName/package.json
   rm "packages/$packageName/package.json.bak"
-  rm "packages/$packageName/package-lock.json"
+  # rm "packages/$packageName/package-lock.json"
   pnpm install && pnpm run build --filter $packageNamespace/$packageName
 }
