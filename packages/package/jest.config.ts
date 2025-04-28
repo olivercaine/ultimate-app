e683a90b21468ab11fad739661ae5f66126f9907
Oliver Caine <olliecaine@gmail.com>
2025-04-29 08:59:10 +1000

    feat(tidy): tidy

--- packages/package/jest.config.ts
+++ packages/package/jest.config.ts
@@ -92,7 +92,7 @@ const config: Config = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  // modulePathIgnorePatterns: ['<rootDir>/dist/'],
 
   // Activates notifications for test results
   // notify: false,
